d227af721ad198cf421caef616c91beb9c1d5ce4
Bianca <bih_goncalves@hotmail.com>
2019-03-17 14:33:59 -0300

    add faq page

---
+++ src/app/faq-page/faq-page.component.ts
@@ -0,0 +1,19 @@
+import { Component, OnInit, Input, ChangeDetectorRef } from '@angular/core';
+
+@Component({
+  selector: 'app-faq-page',
+  templateUrl: './faq-page.component.html',
+  styleUrls: ['./faq-page.component.scss']
+})
+
+export class FaqPageComponent implements OnInit {
+
+//  cdr:ChangeDetectorRef;
+  constructor() {
+  //  this.cdr = cdr;
+  }
+
+  ngOnInit() {
+  }
+
+}
