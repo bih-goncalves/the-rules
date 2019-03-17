3135cdceeadcd3c42a817b072cf2f5f163278531
Bianca <bih_goncalves@hotmail.com>
2019-03-17 15:06:19 -0300

    update app routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,10 +3,18 @@ import { Routes, RouterModule } from '@angular/router';
 
 //ROUTES
 import { HomePageComponent }    from './home-page/home-page.component';
+import { AboutPageComponent }   from './about-page/about-page.component';
+import { FaqPageComponent }     from './faq-page/faq-page.component';
+import { GetInvolvedPageComponent }     from './get-involved-page/get-involved-page.component';
+import { PagePostgrowthComponent } from './page-postgrowth/page-postgrowth.component';
 
 const routes: Routes = [
   { path: '', redirectTo: '/home', pathMatch: 'full' },
-  { path: 'home',  component: HomePageComponent }
+  { path: 'home',  component: HomePageComponent },
+  { path: 'about',  component: AboutPageComponent },
+  { path: 'faq',  component: FaqPageComponent },
+  { path: 'get-involved',  component: GetInvolvedPageComponent },
+  { path: 'postgrowth', component: PagePostgrowthComponent}
 ];
 
 @NgModule({
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -12,11 +12,29 @@ import { HomePageComponent } from './home-page/home-page.component';
 
 //share buttons
 import { ShareModule } from '@ngx-share/core';
+import { AboutPageComponent } from './about-page/about-page.component';
+import { FaqPageComponent } from './faq-page/faq-page.component';
+import { GetInvolvedPageComponent } from './get-involved-page/get-involved-page.component';
+import { SearchableAccordianComponent } from './searchable-accordian/searchable-accordian.component';
+import { BannerComponent } from './banner/banner.component';
+import { VideoOverlayComponent } from './video-overlay/video-overlay.component';
+import { NewsletterSignupComponent } from './newsletter-signup/newsletter-signup.component';
+import { PagePostgrowthComponent } from './page-postgrowth/page-postgrowth.component';
+import { SharableImageComponent } from './sharable-image/sharable-image.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    HomePageComponent
+    HomePageComponent,
+    AboutPageComponent,
+    FaqPageComponent,
+    GetInvolvedPageComponent,
+    SearchableAccordianComponent,
+    BannerComponent,
+    VideoOverlayComponent,
+    NewsletterSignupComponent,
+    PagePostgrowthComponent,
+    SharableImageComponent
   ],
   imports: [
     BrowserModule,
